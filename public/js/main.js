423667aeed1aaa71a63f9c11f6a9331cbc22c450
Corryn Young <young.corryn@gmail.com>
2017-01-31 17:27:19 -0500

    working map markers

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,6 @@
+
+var currentMap;
+
 $(function initializeMap (){
 
   var fullstackAcademy = new google.maps.LatLng(40.705086, -74.009151);
@@ -33,20 +36,88 @@ $(function initializeMap (){
 
   var mapCanvas = document.getElementById('map-canvas');
 
-  var currentMap = new google.maps.Map(mapCanvas, {
+   currentMap = new google.maps.Map(mapCanvas, {
     center: fullstackAcademy,
     zoom: 13,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     styles: styleArr
   });
 
-  var iconURLs = {
+
+
+
+
+  // drawMarker('hotel', [40.705137, -74.007624]);
+  // drawMarker('restaurant', [40.705137, -74.013940]);
+  // drawMarker('activity', [40.716291, -73.995315]);
+
+});
+
+hotels.forEach(function(hotel) {
+  let optionNode = '<option value="' + hotel.id + '">' + hotel.name + '</option>'
+  $('#hotel-choices').append(optionNode);
+});
+
+restaurants.forEach(function(restaurant) {
+  let optionNode = '<option value="' + restaurant.id + '">' + restaurant.name + '</option>'
+  $('#restaurant-choices').append(optionNode);
+});
+
+activities.forEach(function(activity) {
+  let optionNode = '<option value="' + activity.id + '">' + activity.name + '</option>'
+  $('#activity-choices').append(optionNode);
+});
+
+$('#hotel-add').on('click', function(event){
+  let hotelId = $('select#hotel-choices option:selected').val();
+  var index;
+  hotels.forEach(function(hotel, i){
+    if(hotel["id"] == hotelId) index = i;
+  })
+   let hotel = hotels[index].name
+
+  let hoteldiv = '<div class="itinerary-item"> <span class="title">' + hotel + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>'
+  $('#hotel-list').append(hoteldiv);
+  let pos = hotels[index].place.location;
+  drawMarker('hotel', pos);
+
+
+
+})
+
+$('#restaurant-add').on('click', function(event){
+
+  let resId = $('select#restaurant-choices option:checked').val();
+  var index;
+  restaurants.forEach(function(hotel, i){
+    if(restaurant["id"] == resId) index = i;
+  })
+  let restaurant = restaurants[index].name
+  let restaurantdiv = '<div class="itinerary-item"> <span class="title">' + restaurant + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>'
+  $('#restaurant-list').append(restaurantdiv);
+  drawMarker('restaurant', restaurants[index].place.location)
+})
+
+$('#activity-add').on('click', function(event){
+
+  let actId = $('select#activity-choices option:checked').val();
+  var index;
+  activities.forEach(function(activity, i){
+    if(activity["id"] == actId) index = i;
+  })
+  let activity = activities[index].name;
+  let activitydiv = '<div class="itinerary-item"> <span class="title">' + activity + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>'
+  $('#activity-list').append(activitydiv);
+  drawMarker('activity', activities[index].place.location)
+})
+
+var iconURLs = {
     hotel: '/images/lodging_0star.png',
     restaurant: '/images/restaurant.png',
     activity: '/images/star-3.png'
   };
 
-  function drawMarker (type, coords) {
+ function drawMarker (type, coords) {
     var latLng = new google.maps.LatLng(coords[0], coords[1]);
     var iconURL = iconURLs[type];
     var marker = new google.maps.Marker({
@@ -55,9 +126,3 @@ $(function initializeMap (){
     });
     marker.setMap(currentMap);
   }
-
-  drawMarker('hotel', [40.705137, -74.007624]);
-  drawMarker('restaurant', [40.705137, -74.013940]);
-  drawMarker('activity', [40.716291, -73.995315]);
-
-});
